62b3182c816c3b48e4c271f5c82b491a1be106a1
Caio <csc.snf18@uea.edu.br>
2019-06-30 20:59:31 -0400

    teste

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   match "*", to: "home#index", via: :all
   get '/:id', to: "home#index"
+  get '/:id1/:id2', to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
 end
